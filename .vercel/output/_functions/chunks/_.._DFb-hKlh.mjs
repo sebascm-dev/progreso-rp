6d827106a61d661f1334c1d2cd0a51b72388e7d9
sebascm-dev <spanolocabo@gmail.com>
2024-04-03 18:15:06 +0200

    borrar

--- .vercel/output/_functions/chunks/_.._DFb-hKlh.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/__krEfXvfg.mjs').then(n => n._);
-
-export { page };
--- .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/admin_BhdNCR2e.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/admin_DMOV9ZZf.mjs').then(n => n.c);
-
-export { page };
